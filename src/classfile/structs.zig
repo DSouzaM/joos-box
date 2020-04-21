940b34c1de993d6ecff9ec65f95e2a64d256573a
Matt D'Souza <matt.dsouza@gmail.com>
2020-04-21 01:44:05 -0400

    reformat access flags

--- src/classfile/structs.zig
+++ src/classfile/structs.zig
@@ -99,5 +99,12 @@ pub const ConstantPoolInfo = union(ConstantPoolTag) {
 };
 
 pub const AccessFlags = enum(u16) {
-    Public = 0x0001, Final = 0x0010, Super = 0x0020, Interface = 0x0200, Abstract = 0x0400, Synthetic = 0x1000, Annotation = 0x2000, Enum = 0x4000
+    Public = 0x0001,
+    Final = 0x0010,
+    Super = 0x0020,
+    Interface = 0x0200,
+    Abstract = 0x0400,
+    Synthetic = 0x1000,
+    Annotation = 0x2000,
+    Enum = 0x4000,
 };
